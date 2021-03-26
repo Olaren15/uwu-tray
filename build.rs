b4926e590bbeefdce9f84a1ac6921cf8fe97c6ca
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-03-25 23:42:07 -0400

    Added winux suppowt

--- build.rs
+++ build.rs
@@ -6,5 +6,12 @@ fn main() {
     windres::Build::new().compile("res/res.rc").unwrap();
 }
 
+use std::process::Command;
+
 #[cfg(unix)]
-fn main() {}
+fn main() {
+    Command::new("sh")
+        .args(&["-c", "cd res && glib-compile-resources res.xml"])
+        .output()
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,42 @@
 use tray_item::TrayItem;
 use clipboard::{ClipboardProvider, ClipboardContext};
 use std::str;
+use gtk::IconThemeExt;
 
 #[inline(always)]
 fn round_up(a: usize, b: usize) -> usize { (a + b - 1) / b * b }
 
+fn init() {
+    #[cfg(target_os = "linux")]
+        {
+            gtk::init().unwrap();
+            let resources_bytes = include_bytes!("../res/res.gresource");
+            let resource_data = glib::Bytes::from(&resources_bytes[..]);
+            let res = gio::Resource::new_from_data(&resource_data).unwrap();
+            gio::resources_register(&res);
+            gtk::IconTheme::get_default().unwrap().add_resource_path("/dev/olaren/uwu-tray");
+        }
+}
+
+fn quit() {
+    #[cfg(target_os = "windows")]
+        std::process::exit(0);
+
+    #[cfg(target_os = "linux")]
+        gtk::main_quit();
+}
+
+fn main_loop() {
+    #[cfg(target_os = "windows")]
+        std::io::stdin().read_line(&mut String::new()).unwrap();
+
+    #[cfg(target_os = "linux")]
+        gtk::main();
+}
+
 fn main() {
+    init();
+
     let mut tray = TrayItem::new("uwu", "uwu").expect("Failed to create tray icon");
 
     tray.add_menu_item("uwuify", || {
@@ -24,12 +55,11 @@ fn main() {
 
             clipboard.set_contents(String::from(str::from_utf8(res_bytes).unwrap())).unwrap();
         }
-        println!("uwu");
     }).unwrap();
 
     tray.add_menu_item("Quit", || {
-        std::process::exit(0);
+        quit();
     }).unwrap();
 
-    loop {}
+    main_loop();
 }
