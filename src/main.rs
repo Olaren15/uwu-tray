ee702c30876f2a713fe8d720bf9de2e7884e7d22
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-03-25 23:59:22 -0400

    Updated uwuify api u-usage

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@
 
 use tray_item::TrayItem;
 use clipboard::{ClipboardProvider, ClipboardContext};
-use std::str;
 
 #[cfg(target_os = "linux")]
 use gtk::IconThemeExt;
-
-#[inline(always)]
-fn round_up(a: usize, b: usize) -> usize { (a + b - 1) / b * b }
 
 fn init() {
     #[cfg(target_os = "linux")]
@@ -47,15 +43,7 @@ fn main() {
         let mut clipboard: ClipboardContext = ClipboardProvider::new().unwrap();
 
         if let Ok(contents) = clipboard.get_contents() {
-            let mut temp_bytes1 = [0u8; 1000000];
-            let mut temp_bytes2 = [0u8; 1000000];
-
-            let mut bytes = contents.as_bytes().to_owned();
-            let len = bytes.len();
-            bytes.resize(round_up(len, 16), 0);
-            let res_bytes = uwuifier::uwu_ify_sse(&bytes, len, &mut temp_bytes1, &mut temp_bytes2);
-
-            clipboard.set_contents(String::from(str::from_utf8(res_bytes).unwrap())).unwrap();
+            clipboard.set_contents(uwuifier::uwuify_str_sse(contents.as_str())).unwrap();
         }
     }).unwrap();
 
