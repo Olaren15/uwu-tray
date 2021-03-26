150cc01424da6cb52225486cbec422b9cd4f8247
MikhailMS <1488maiklm@gmail.com>
2021-03-26 15:49:58 +0000

    Add support for macOS

--- build.rs
+++ build.rs
@@ -16,3 +16,23 @@ fn main() {
         .output()
         .unwrap();
 }
+
+#[cfg(target_os="macos")]
+use std::{fs, process::Command};
+
+#[cfg(target_os="macos")]
+fn main() {
+    println!("Building uwu-tray MacOS .app package");
+    Command::new("cargo").arg("build").arg("--release").arg("--package=uwu-tray");
+
+    println!("packing uwu-tray.dmg");
+    fs::create_dir_all("assets/macos/uwu-tray.app/Contents/MacOS").expect("Cannot create [MacOS] directory for MacOS app");
+    fs::create_dir_all("assets/macos/uwu-tray.app/Contents/Resources").expect("Cannot create [Resources] directory for MacOS app");
+
+    fs::copy("target/release/uwu-tray", "assets/macos/uwu-tray.app/Contents/MacOS/uwu-tray").expect("Cannot copy uwu-tray binary into MacOS app");
+    fs::copy("res/uwu.icns",            "assets/macos/uwu-tray.app/Contents/Resources/AppIcon.icns").expect("Cannot copy icon.icns into Resources");
+    fs::copy("res/Info.plist",          "assets/macos/uwu-tray.app/Contents/Info.plist").expect("Cannot copy Info.plist into MacOS package");
+    
+    Command::new("hdiutil").arg("create").arg("assets/uwu-tray.dmg").arg("-volname").arg("uwu-tray").arg("-srcfolder").arg("assets/macos").arg("-ov");
+    println!("Package build is completed");
+}
--- src/main.rs
+++ src/main.rs
@@ -6,50 +6,70 @@ use clipboard::{ClipboardProvider, ClipboardContext};
 #[cfg(target_os = "linux")]
 use gtk::IconThemeExt;
 
+#[cfg(target_os = "linux")]
 fn init() {
-    #[cfg(target_os = "linux")]
-        {
-            gtk::init().unwrap();
-            let resources_bytes = include_bytes!("../res/res.gresource");
-            let resource_data = glib::Bytes::from(&resources_bytes[..]);
-            let res = gio::Resource::new_from_data(&resource_data).unwrap();
-            gio::resources_register(&res);
-            gtk::IconTheme::get_default().unwrap().add_resource_path("/dev/olaren/uwu-tray");
-        }
+    gtk::init().unwrap();
+    let resources_bytes = include_bytes!("../res/res.gresource");
+    let resource_data = glib::Bytes::from(&resources_bytes[..]);
+    let res = gio::Resource::new_from_data(&resource_data).unwrap();
+    gio::resources_register(&res);
+    gtk::IconTheme::get_default().unwrap().add_resource_path("/dev/olaren/uwu-tray");
 }
 
 fn quit() {
-    #[cfg(target_os = "windows")]
-        std::process::exit(0);
+    #[cfg(any(target_os = "windows", target_os="macos"))]
+    std::process::exit(0);
 
     #[cfg(target_os = "linux")]
-        gtk::main_quit();
+    gtk::main_quit();
 }
 
+#[cfg(any(target_os = "windows", target_os="linux"))]
 fn main_loop() {
     #[cfg(target_os = "windows")]
-        std::io::stdin().read_line(&mut String::new()).unwrap();
+    std::io::stdin().read_line(&mut String::new()).unwrap();
 
     #[cfg(target_os = "linux")]
-        gtk::main();
+    gtk::main();
 }
 
+#[cfg(any(target_os="windows", target_os="linux"))]
 fn main() {
     init();
 
     let mut tray = TrayItem::new("uwu", "uwu").expect("Failed to create tray icon");
 
     tray.add_menu_item("uwuify", || {
-        let mut clipboard: ClipboardContext = ClipboardProvider::new().unwrap();
+        let mut clipboard: ClipboardContext = ClipboardProvider::new().expect("Failed to create clipboard context");
 
         if let Ok(contents) = clipboard.get_contents() {
-            clipboard.set_contents(uwuifier::uwuify_str_sse(contents.as_str())).unwrap();
+            clipboard.set_contents(uwuifier::uwuify_str_sse(contents.as_str())).expect("Failed to set clipboard contents");
         }
-    }).unwrap();
+    }).expect("Failed to add menu item");
 
     tray.add_menu_item("Quit", || {
         quit();
-    }).unwrap();
+    }).expect("Failed to add menu item");
 
     main_loop();
+}
+
+#[cfg(target_os="macos")]
+fn main() {
+    let mut tray = TrayItem::new("uwu", "uwu").expect("Failed to create tray icon");
+
+    tray.add_menu_item("uwuify", || {
+        let mut clipboard: ClipboardContext = ClipboardProvider::new().expect("Failed to create clipboard context");
+
+        if let Ok(contents) = clipboard.get_contents() {
+            clipboard.set_contents(uwuifier::uwuify_str_sse(contents.as_str())).expect("Failed to set clipboard contents");
+        }
+    }).expect("Failed to add menu item");
+
+
+    tray.add_menu_item("Quit", || {
+        quit();
+    }).expect("Failed to add menu item");
+    
+    tray.inner_mut().display();
 }
