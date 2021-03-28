c845925568d02b166cd029407d3c6e6dc5997b13
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-03-28 17:00:14 -0400

    Merged duplicated code + fixed compilation on windows

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,65 @@
 #![windows_subsystem = "windows"]
 
+use clipboard::{ClipboardContext, ClipboardProvider};
 use tray_item::TrayItem;
-use clipboard::{ClipboardProvider, ClipboardContext};
 
 #[cfg(target_os = "linux")]
 use gtk::IconThemeExt;
 
-#[cfg(target_os = "linux")]
 fn init() {
-    gtk::init().unwrap();
-    let resources_bytes = include_bytes!("../res/res.gresource");
-    let resource_data = glib::Bytes::from(&resources_bytes[..]);
-    let res = gio::Resource::new_from_data(&resource_data).unwrap();
-    gio::resources_register(&res);
-    gtk::IconTheme::get_default().unwrap().add_resource_path("/dev/olaren/uwu-tray");
+    #[cfg(target_os = "linux")]
+    {
+        gtk::init().unwrap();
+        let resources_bytes = include_bytes!("../res/res.gresource");
+        let resource_data = glib::Bytes::from(&resources_bytes[..]);
+        let res = gio::Resource::new_from_data(&resource_data).unwrap();
+        gio::resources_register(&res);
+        gtk::IconTheme::get_default()
+            .unwrap()
+            .add_resource_path("/dev/olaren/uwu-tray");
+    }
 }
 
 fn quit() {
-    #[cfg(any(target_os = "windows", target_os="macos"))]
+    #[cfg(any(target_os = "windows", target_os = "macos"))]
     std::process::exit(0);
 
     #[cfg(target_os = "linux")]
     gtk::main_quit();
 }
 
-#[cfg(any(target_os = "windows", target_os="linux"))]
 fn main_loop() {
     #[cfg(target_os = "windows")]
     std::io::stdin().read_line(&mut String::new()).unwrap();
 
     #[cfg(target_os = "linux")]
     gtk::main();
+
+    #[cfg(target_os = "macos")]
+    tray.inner_mut().display();
 }
 
-#[cfg(any(target_os="windows", target_os="linux"))]
 fn main() {
     init();
 
     let mut tray = TrayItem::new("uwu", "uwu").expect("Failed to create tray icon");
 
     tray.add_menu_item("uwuify", || {
-        let mut clipboard: ClipboardContext = ClipboardProvider::new().expect("Failed to create clipboard context");
+        let mut clipboard: ClipboardContext =
+            ClipboardProvider::new().expect("Failed to create clipboard context");
 
         if let Ok(contents) = clipboard.get_contents() {
-            clipboard.set_contents(uwuifier::uwuify_str_sse(contents.as_str())).expect("Failed to set clipboard contents");
+            clipboard
+                .set_contents(uwuifier::uwuify_str_sse(contents.as_str()))
+                .expect("Failed to set clipboard contents");
         }
-    }).expect("Failed to add menu item");
+    })
+    .expect("Failed to add menu item");
 
     tray.add_menu_item("Quit", || {
         quit();
-    }).expect("Failed to add menu item");
+    })
+    .expect("Failed to add menu item");
 
     main_loop();
-}
-
-#[cfg(target_os="macos")]
-fn main() {
-    let mut tray = TrayItem::new("uwu", "uwu").expect("Failed to create tray icon");
-
-    tray.add_menu_item("uwuify", || {
-        let mut clipboard: ClipboardContext = ClipboardProvider::new().expect("Failed to create clipboard context");
-
-        if let Ok(contents) = clipboard.get_contents() {
-            clipboard.set_contents(uwuifier::uwuify_str_sse(contents.as_str())).expect("Failed to set clipboard contents");
-        }
-    }).expect("Failed to add menu item");
-
-
-    tray.add_menu_item("Quit", || {
-        quit();
-    }).expect("Failed to add menu item");
-    
-    tray.inner_mut().display();
 }
